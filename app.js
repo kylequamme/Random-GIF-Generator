df4469a4f4d15cb930f9ddf4643449da6ef4e25e
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-07-04 13:19:40 -0500

    fixed images and links

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ $(function(){
     $('main').empty();
     $.ajax('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + tag).success(function(giphy){
         $('main').append('<span class="result"><img src="' + giphy.data.image_url + '" />');
-        $('main').append('<p><a href="' + giphy.data.image_url + '">' + giphy.data.image_url + '</a></p>');
+        $('main').append('<p><a href="' + giphy.data.image_url + '" target="_blank">' + giphy.data.image_url + '</a></p>');
       }
       )
       .fail(function(){
@@ -28,8 +28,8 @@ $(function(){
       $('main').empty();
       $.ajax('http://api.giphy.com/v1/stickers/random?api_key=dc6zaTOxFJmzC&tag=' + tag).success(function(giphy){
         $('main').append('<span class="result"></span>');
-        $('.result').append('<img src="' + giphy.data.image_url + '" width="500px"/>');
-        $('.result').append('<p><a href="' + giphy.data.image_url + '">' + giphy.data.image_url + '</a></p>');
+        $('.result').append('<img src="' + giphy.data.image_url + '" />');
+        $('.result').append('<p><a href="' + giphy.data.image_url + '" target="_blank">' + giphy.data.image_url + '</a></p>');
       }
       )
         .fail(function(){
