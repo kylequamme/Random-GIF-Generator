5e4b01799b985d87f8cdcce0a6649366e7b9ba24
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-07-04 13:03:35 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+//Author: Kyle Quamme
+//Created: 6/30/16
+$(function(){
+  var requestedMovie = '';
+
+  $('.btnGif').on('click', function(){
+    requestedTag = $('#searchBox').val().split(' ').join('+');
+    getGIF(requestedTag);
+  });
+  $('.btnSticker').on('click', function(){
+    requestedTag = $('#searchBox').val().split(' ').join('-');
+    getStickers(requestedTag);
+  });
+
+  function getGIF(tag){
+    $('main').empty();
+    $.ajax('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + tag).success(function(giphy){
+        $('main').append('<span class="result"><img src="' + giphy.data.image_url + '" />');
+        $('main').append('<p><a href="' + giphy.data.image_url + '">' + giphy.data.image_url + '</a></p>');
+      }
+      )
+      .fail(function(){
+        $('main').append('<span><h2>Failed to load data from API</h2>');
+      }
+      );
+    }
+    function getStickers(tag){
+      $('main').empty();
+      $.ajax('http://api.giphy.com/v1/stickers/random?api_key=dc6zaTOxFJmzC&tag=' + tag).success(function(giphy){
+        $('main').append('<span class="result"></span>');
+        $('.result').append('<img src="' + giphy.data.image_url + '" width="500px"/>');
+        $('.result').append('<p><a href="' + giphy.data.image_url + '">' + giphy.data.image_url + '</a></p>');
+      }
+      )
+        .fail(function(){
+          $('.result').append('<h2>Failed to load data from API</h2>');
+        }
+        );
+      }
+});
